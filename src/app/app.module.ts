682e99e0ee3c2769761429d5676d799c4a3a721a
dantheman0124 <daniel.bassani@outlook.com>
2022-05-18 16:50:23 -0400

    sidebar and main content progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule
