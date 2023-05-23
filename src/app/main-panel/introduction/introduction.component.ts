68d80a28e6706884118f0ba3ad57cee9f82dadc1
Daniel Bassani <39228138+danielbassani@users.noreply.github.com>
2023-05-23 16:44:46 -0400

    all relevant changes

--- src/app/main-panel/introduction/introduction.component.ts
+++ src/app/main-panel/introduction/introduction.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class IntroductionComponent implements OnInit {
 
   public introduction:string = 
-    'A 5th year software development student currently in my final year with practical on-the-job experience developing cloud based software solutions. '
+    'A graduate from Queen\'s University with a Bachelor degree in Computer Science and specialization in software development. I have practical on-the-job experience developing cloud based software solutions. '
     + 'Experience working collaboratively in a team and meeting tight deadlines, along with utilizing problem-solving skills to think of quick solutions '
     + 'to problems. Excited to apply acquired skills on large-scale corporate solutions and eager to take initiative to learn new skills on the job as needed.';
 
